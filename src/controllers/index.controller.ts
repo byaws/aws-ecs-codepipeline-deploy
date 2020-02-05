2b67fd14e426c4a656aea50bc2e3ce23af1d1a4e
임경민 <ljlm0402@naver.com>
2020-02-05 16:00:13 +0900

    Fix ec2 auto scaling

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -4,7 +4,7 @@ class IndexController {
 
   public index = (req: Request, res: Response, next: NextFunction) => {
     try {
-      res.status(200).json({ message: 'autuScaling' });
+      res.status(200).json({ message: 'EC2 auto scaling' });
     } catch (error) {
       next(error);
     }
